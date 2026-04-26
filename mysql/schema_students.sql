534adccff03cccde2d4bf0ff6a8c11c0a4460206
achsah-12 <aksahruth@gmail.com>
2026-04-26 07:19:19 +0300

    Inserted student sample data

--- mysql/schema_students.sql
+++ mysql/schema_students.sql
@@ -6,4 +6,4 @@ INSERT INTO Student (first_name, last_name, email, phone, grade) VALUES
 ('Tigist', 'Haile', 'tigist.haile@school.com', '0922345678', 'Grade 10'),
 ('Dawit', 'Tadesse', 'dawit.tadesse@school.com', '0933456789', 'Grade 12'),
 ('Selam', 'Girma', 'selam.girma@school.com', '0944567890', 'Grade 11'),
-('Yonas', 'Bekele', 'yonas.bekele@school.com', '0955678901', 'Grade 10');    
+('Yonas', 'Bekele', 'yonas.bekele@school.com', '0955678901', 'Grade 10');  
