63cb8e4852805b68620ba8cfcd5646d51fdf8d02
achsah-12 <aksahruth@gmail.com>
2026-04-26 07:14:58 +0300

    Create schema_students.sql

---
+++ mysql/schema_students.sql
@@ -0,0 +1,9 @@
+USE school_library;
+
+-- Insert Students
+INSERT INTO Student (first_name, last_name, email, phone, grade) VALUES
+('Abebe', 'Kebede', 'abebe.kebede@school.com', '0911234567', 'Grade 11'),
+('Tigist', 'Haile', 'tigist.haile@school.com', '0922345678', 'Grade 10'),
+('Dawit', 'Tadesse', 'dawit.tadesse@school.com', '0933456789', 'Grade 12'),
+('Selam', 'Girma', 'selam.girma@school.com', '0944567890', 'Grade 11'),
+('Yonas', 'Bekele', 'yonas.bekele@school.com', '0955678901', 'Grade 10');    
